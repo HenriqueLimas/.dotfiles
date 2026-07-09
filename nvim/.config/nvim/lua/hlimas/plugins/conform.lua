489d5e8839326fff7a4d3fa207a1433a0ff23cd0
HenriqueLimas <henrique.ramos.limas@gmail.com>
2026-07-09 15:28:38 -0700

    nvim: only run prettier/prettierd when a config file exists

--- nvim/.config/nvim/lua/hlimas/plugins/conform.lua
+++ nvim/.config/nvim/lua/hlimas/plugins/conform.lua
@@ -1,3 +1,23 @@
+local prettier_configs = {
+	".prettierrc",
+	".prettierrc.js",
+	".prettierrc.cjs",
+	".prettierrc.mjs",
+	".prettierrc.json",
+	".prettierrc.json5",
+	".prettierrc.yaml",
+	".prettierrc.yml",
+	".prettierrc.toml",
+	"prettier.config.js",
+	"prettier.config.cjs",
+	"prettier.config.mjs",
+	"prettier.config.ts",
+}
+
+local function has_prettier_config(ctx)
+	return vim.fs.find(prettier_configs, { path = ctx.filename, upward = true })[1] ~= nil
+end
+
 return {
 	"stevearc/conform.nvim",
 	opts = {},
@@ -12,8 +32,11 @@ return {
 				typescriptreact = { "prettierd", "prettier", "eslint_d", stop_after_first = true },
 				markdown = { "prettierd", "prettier", "eslint_d", stop_after_first = true },
 			},
+			formatters = {
+				prettierd = { condition = has_prettier_config },
+				prettier = { condition = has_prettier_config },
+			},
 			format_on_save = {
-				-- These options will be passed to conform.format()
 				timeout_ms = 500,
 			},
 		})
