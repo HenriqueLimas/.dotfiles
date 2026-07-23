cd440c5a7c0719673c2edc4cdb223919e815e23a
HenriqueLimas <henrique.ramos.limas@gmail.com>
2026-07-23 14:32:05 -0700

    update theme

--- nvim/.config/nvim/lua/hlimas/plugins/colors.lua
+++ nvim/.config/nvim/lua/hlimas/plugins/colors.lua
@@ -1,24 +1,15 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		config = function()
-			require('rose-pine').setup({
-				disable_background = true,
+			require("rose-pine").setup({
 				styles = {
 					italic = false,
 				},
 			})
 
-			ColorMyPencils();
-		end
+			vim.cmd.colorscheme("rose-pine")
+		end,
 	},
 }
