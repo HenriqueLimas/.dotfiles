bb760ecdf9f30067d5d270f8f477d4688a07df93
HenriqueLimas <henrique.ramos.limas@gmail.com>
2026-08-03 14:57:26 -0700

    nvim: migrate treesitter configuration

--- nvim/.config/nvim/lua/hlimas/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/hlimas/plugins/treesitter.lua
@@ -1,50 +1,54 @@
+local languages = {
+	"c",
+	"lua",
+	"vim",
+	"vimdoc",
+	"query",
+	"markdown",
+	"markdown_inline",
+	"javascript",
+	"typescript",
+	"go",
+	"marko",
+}
+
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
 		build = ":TSUpdate",
+		init = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "TSUpdate",
+				callback = function()
+					require("nvim-treesitter.parsers").marko = {
+						install_info = {
+							path = vim.fn.expand("~/Development/github/marko-tree-sitter/main"),
+							queries = "queries",
+						},
+					}
+				end,
+			})
+		end,
 		config = function()
-			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			parser_config.marko = {
-				install_info = {
-					url = "/Users/hlimas/Development/github/marko-tree-sitter",
-					files = { "src/parser.c", "src/scanner.c" },
-					generate_requires_npm = true,
-				},
-				filetype = "marko",
-			}
+			require("nvim-treesitter").install(languages)
 
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				ensure_installed = {
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
 					"c",
 					"lua",
 					"vim",
 					"vimdoc",
 					"query",
 					"markdown",
-					"markdown_inline",
 					"javascript",
 					"typescript",
 					"go",
 					"marko",
 				},
-
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = false,
-
-				-- Automatically install missing parsers when entering buffer
-				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-				auto_install = true,
-
-				highlight = {
-					enable = true,
-
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = false,
-				},
+				callback = function()
+					vim.treesitter.start()
+				end,
 			})
 		end,
 	},
